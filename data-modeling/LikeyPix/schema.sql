b413322f57af91a2e8c327020d26514b313185e2
Gabriel Jefferson <phantom8528@gmail.com>
2022-05-05 13:31:05 -0400

    week13-branch5: In-Class Learning

--- data-modeling/LikeyPix/schema.sql
+++ data-modeling/LikeyPix/schema.sql
@@ -9,3 +9,23 @@ create table posts(
     url text not null,
     user_id integer references users (id)
 );
+
+-- Creating the Tags Table
+create table tags(
+    id serial primary key,
+    tag varchar(50) not null
+);
+
+--Create Tabs_Posts Table
+-- -------------------------------
+-- The purpose of a linking table is to associate the rows of two tables
+-- --> Only have foreign keys
+-- --> Are named for the two tables they link
+
+create table tags_posts (
+    tag_id integer references tags (id),
+    post_id integer references posts (id)
+);
+
+-- 05/05/22
+-- Created a pics table in the terminal with the one column url
